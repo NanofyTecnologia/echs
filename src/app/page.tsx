73083e12d76886e6eac0926980f3617568b8b543
ferpereira <fernando.luporini@hotmail.com>
2025-02-20 13:19:21 -0300

    fix: corrige problemas de css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 'use client'
 
-// import Welcome from '@/components/_Sections/Welcome'
-// import AboutUs from '@/components/_Sections/AboutUs'
-// import Services from '@/components/_Sections/Services'
-// import Contact from '@/components/_Sections/Contact'
-// import Map from '@/components/_Sections/Map'
-// import Customers from '@/components/_Sections/Customers'
-// import Form from '@/components/_Sections/Form'
+import AboutUs from '@/components/_Sections/AboutUs'
+import Contact from '@/components/_Sections/Contact'
+import Customers from '@/components/_Sections/Customers'
+import Form from '@/components/_Sections/Form'
+import Map from '@/components/_Sections/Map'
+import Services from '@/components/_Sections/Services'
+import Welcome from '@/components/_Sections/Welcome'
 
-export default function HomePage() {
-  return <div></div>
+export default function Content() {
+  return (
+    <>
+      <Welcome />
+      <AboutUs />
+      <Services />
+      <Customers />
+      <Contact />
+      <Form />
+      <Map />
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: 'var(--font-poppins)',
-        alt: ['poppins'],
-      },
       colors: {
         primary: '#e75c4f',
         secondary: '#28375D',
@@ -30,4 +21,3 @@ const config: Config = {
   },
   plugins: [],
 }
-export default config
