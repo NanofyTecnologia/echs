36cec6dcd2d4470e840fbbc6cf88e04245121dd9
Fernando Pereira <ferpereira@MacBook-Air-de-Fernando.local>
2025-11-19 14:29:19 -0300

    fix: adiciona tag de srcipt para permissao do google analytics

--- next.config.js
+++ next.config.js
@@ -1,37 +1,41 @@
-/** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: `
+      default-src 'self';
 
-const nextConfig = {
+      script-src
+        'self'
+        'unsafe-inline'
+        https://www.googletagmanager.com
+        https://www.google-analytics.com;
+
+      script-src-elem
+        'self'
+        https://www.googletagmanager.com
+        https://www.google-analytics.com;
+
+      img-src
+        'self'
+        data:
+        https://www.google-analytics.com
+        https://www.googletagmanager.com;
+
+      connect-src
+        'self'
+        https://www.google-analytics.com
+        https://www.googletagmanager.com;
+
+      frame-src https://www.googletagmanager.com;
+      style-src 'self' 'unsafe-inline';
+    `
+      .replace(/\s{2,}/g, ' ')
+      .trim(),
+  },
+]
+
+module.exports = {
   async headers() {
-    const securityHeaders = [
-      {
-        key: 'Content-Security-Policy',
-        value: `
-          default-src 'self';
-
-          script-src-elem 
-            'self'
-            'unsafe-inline'
-            https://www.googletagmanager.com
-            https://www.google-analytics.com;
-
-          img-src
-            'self'
-            data:
-            https://www.google-analytics.com;
-
-          connect-src
-            'self'
-            https://www.google-analytics.com
-            https://www.googletagmanager.com;
-
-          style-src 'self' 'unsafe-inline';
-          frame-src https://www.googletagmanager.com;
-        `
-          .replace(/\s{2,}/g, ' ')
-          .trim(),
-      },
-    ]
-
     return [
       {
         source: '/(.*)',
@@ -40,5 +44,3 @@ const nextConfig = {
     ]
   },
 }
-
-module.exports = nextConfig
