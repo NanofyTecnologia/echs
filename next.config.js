852706a0ef34a2d0b9af243b3bd62d435061f324
Fernando Pereira <ferpereira@MacBook-Air-de-Fernando.local>
2025-11-19 14:26:37 -0300

    fix: corrige erro de seguranca csp do google analytics

--- next.config.js
+++ next.config.js
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  async headers() {
+    const securityHeaders = [
+      {
+        key: 'Content-Security-Policy',
+        value: `
+          default-src 'self';
+
+          script-src-elem 
+            'self'
+            'unsafe-inline'
+            https://www.googletagmanager.com
+            https://www.google-analytics.com;
+
+          img-src
+            'self'
+            data:
+            https://www.google-analytics.com;
+
+          connect-src
+            'self'
+            https://www.google-analytics.com
+            https://www.googletagmanager.com;
+
+          style-src 'self' 'unsafe-inline';
+          frame-src https://www.googletagmanager.com;
+        `
+          .replace(/\s{2,}/g, ' ')
+          .trim(),
+      },
+    ]
+
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
